be8911f0820dd76efbb4fc0c38260a7acdaa3404
Oswaldo <oswaldo.vazquez@gmail.com>
2019-08-21 09:50:32 -0500

    hamburger working

--- app.js
+++ app.js
@@ -100,22 +100,55 @@ function init() {
     }
 
 
+
+    function throttle(func, limit) {
+        let inThrottle;
+        return function() {
+            const args = arguments;
+            const context = this;
+            if (!inThrottle) {
+                func.apply(context, args);
+                inThrottle = true;
+                setTimeout(() => (inThrottle = false), limit)
+            }
+        }
+    }
+
+
+    const hamburger = document.querySelector('.menu');
+    const hamburgerLines = document.querySelectorAll('.menu line');
+    const navOpen = document.querySelector('.nav-open');
+    const contact = document.querySelector('.contact');
+    const social = document.querySelector('.social');
+    const logo = document.querySelector('.logo');
+
+    const tl = new TimelineMax({paused: true, reversed: TextTrackCue});
+
+    tl.to(navOpen, 0.5, { y: 0}).fromTo(
+        contact,
+        0.5,
+        { opacity: 0, y: 10},
+        { opacity: 1, y: 0}, '-= 0.1'
+    )
+    .fromTo(
+        social,
+        0.5,
+        { opacity: 0, y: 10},
+        { opacity: 1, y: 0}, '-=0.5'
+    )
+    .fromTo(logo, 0.2, {color: 'white'}, {color: 'black'}, '-=1')
+    .fromTo(hamburgerLines, 0.2, {stroke: "white"}, {stroke: "black"}, "-=1");
+
+
+    hamburger.addEventListener('click', () => {
+        tl.reversed() ? tl.play() : tl.reverse();
+    })
+
    
 }
 
 
 
-function throttle(func, limit) {
-    let inThrottle;
-    return function() {
-        const args = arguments;
-        const context = this;
-        if (!inThrottle) {
-            func.apply(context, args);
-            inThrottle = true;
-            setTimeout(() => (inThrottle = false), limit)
-        }
-    }
-}
+
 
 init();
